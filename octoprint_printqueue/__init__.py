6e09f150cb669e99b82e95abdd7cc0794b96540d
Kenneth Jiang <kenneth.jiang@gmail.com>
2018-10-30 05:40:22 -0700

    wrong url

--- octoprint_printqueue/__init__.py
+++ octoprint_printqueue/__init__.py
@@ -34,7 +34,7 @@ class PrintQueuePlugin(octoprint.plugin.SettingsPlugin,
 
 	def get_settings_defaults(self):
 		return dict(
-                        endpoint_prefix="https://app.gofab.com/"
+                        endpoint_prefix="https://app.gofab.xyz/"
 		)
 
 	##~~ AssetPlugin mixin
